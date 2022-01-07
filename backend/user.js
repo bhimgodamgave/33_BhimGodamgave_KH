675c9ea4b77bcb0604926d546e8e0e8dc3610980
bhimgodamgave <96038432+bhimgodamgave@users.noreply.github.com>
2022-01-07 17:48:58 +0530

    Add files via upload

--- backend/user.js
+++ backend/user.js
@@ -1,1 +1,32 @@
+const mysql = require("mysql");
+const Promise = require("bluebird");
+ Promise.promisifyAll(require("mysql/lib/Connection").prototype);
 
+ const dbinfo ={
+     host : "localhost",
+     user : "root",
+     password : "cdac",
+     database : "project1",
+ }
+
+ const selectMessage = async() =>{
+     const Connection = mysql.createConnection(dbinfo);
+     await Connection.connectAsync();
+     let sql = "select * from user";
+     let list = await Connection.queryAsync(sql);
+     await Connection.endAsync();
+     return list;
+     
+ }
+
+ const addMessage = async(user) =>{
+    const Connection = mysql.createConnection(dbinfo);
+    await Connection.connectAsync();
+    let sql = "insert into user (message) values (?)";
+    await Connection.queryAsync(sql,[user.message]);
+    console.log("message added");
+    await Connection.endAsync();
+    
+ }
+
+ module.exports = {selectMessage,addMessage};
